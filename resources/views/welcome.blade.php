71937fafcb2e9ef888042af092208193b17fb2a1
darkheart101 <kurokokoro@windowslive.com>
2019-11-17 12:09:06 +0200

    front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Appointment System API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -13,86 +13,139 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: #000;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
-                margin: 0;
+                margin: 1%;
             }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <div >
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <h1>Appointment System API</h1>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p>The Appointment System API will help your keep your contacts organized and arrange appointments</p>
+                <h2>Documentation</h2>
+                <table style="width:100%">
+                    <tr>
+                        <th style="text-align:left;">Api Call</th>
+                        <th style="text-align:left;">Http Method</th>
+                        <th style="text-align:left;">Parameters</th>
+                        <th style="text-align:left;">Description</th>
+                    </tr>
+
+                    <tr>
+                        <td>/api/login</td>
+                        <td>POST</td>
+                        <td>UsrEmail, UsrPassword</td>
+                        <td>Logs the user in and returns a token</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/add_user</td>
+                        <td>POST</td>
+                        <td>token, UsrFirstname, UsrLastname, UsrEmail, UsrPassword, UsrRoleID</td>
+                        <td>creates new user</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/update_user</td>
+                        <td>PUT</td>
+                        <td>token, UsrFirstname, UsrLastname, UsrEmail, UsrPassword, UsrRoleID</td>
+                        <td>updates new user</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/contacts</td>
+                        <td>GET</td>
+                        <td>token</td>
+                        <td>Lists all contacts</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/add_contact</td>
+                        <td>POST</td>
+                        <td>token, ContactFirstname, ContactLastname, ContactEmail, ContactAddress, ContactCity, ContactCellphone</td>
+                        <td>creates new contact</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/contact/{id}</td>
+                        <td>PUT</td>
+                        <td>token, ContactFirstname, ContactLastname, ContactEmail, ContactAddress, ContactCity, ContactCellphone</td>
+                        <td>updates new contact</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/contact/{id}</td>
+                        <td>GET</td>
+                        <td>token, id</td>
+                        <td>returns a contact contact</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/contact/{id}</td>
+                        <td>DELETE</td>
+                        <td>token, id</td>
+                        <td>deletes a contact contact</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/add_appointment</td>
+                        <td>POST</td>
+                        <td>token, ApntDate, ApntContactID, Comments</td>
+                        <td>creates new appointment</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/appointment/{id}</td>
+                        <td>GET</td>
+                        <td>token, id</td>
+                        <td>returns an appointment</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/appointment/{id}</td>
+                        <td>PUT</td>
+                        <td>token, id, ApntDate, ApntContactID, Comments</td>
+                        <td>updates an appointment</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/appointment/{id}</td>
+                        <td>DELETE</td>
+                        <td>token, id</td>
+                        <td>deletes an appointment</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/appointments</td>
+                        <td>GET</td>
+                        <td>token</td>
+                        <td>returns list of appointments</td>
+                    </tr>
+
+                    <tr>
+                        <td>/api/logout</td>
+                        <td>POST</td>
+                        <td>token</td>
+                        <td>logs the user out</td>
+                    </tr>
+                </table>
+
+                <p>User with role id <b>1</b> is always the system administrator</p>
+
+                <h3>Additional Information</h3>
+                <p>
+                    The sole intent of this application is to study the Test Driven Development method.
+                    For the purpose in the test folder you can find all the test that were used for
+                    the Development of this application.
+                </p>
+
             </div>
         </div>
     </body>
