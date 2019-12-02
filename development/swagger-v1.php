e50aec8493b3e4116a80439b245edcf7e3c5b028
darkheart101 <kurokokoro@windowslive.com>
2019-12-02 05:02:44 +0000

    chore swagger installation

---
+++ development/swagger-v1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host=API_HOST,
+ *     basePath="/",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Swagger Integration with PHP Laravel",
+ *         description="Integrate Swagger in Laravel application",
+ *         termsOfService="",
+ *         @SWG\Contact(
+ *             email="sachit.wadhawan@quovantis.com"
+ *         ),
+ *     ),
+ * )
+ */
